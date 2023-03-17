cb8cfc14833ec782a527bdf2f45dafd292858057
Erie Mae Segui <qemcsegui@tip.edu.ph>
2023-03-17 09:30:10 +0800

    Added weather api in V1 branch

--- app/src/main/java/com/example/sikligtas/ui/MainActivity.kt
+++ app/src/main/java/com/example/sikligtas/ui/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.sikligtas.ui
 
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
@@ -18,6 +23,8 @@ import com.example.sikligtas.databinding.ActivityMainBinding
 import com.example.sikligtas.util.Permissions.hasLocationPermission
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,6 +37,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var actionBarToggle: ActionBarDrawerToggle
 
     private lateinit var firebaseAuth: FirebaseAuth
+
+    val city: String = "Manila, phl"
+    val weatherAPI: String = "6e15e38580b41e39d9b85ee972e63c49"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,6 +79,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, SignInActivity::class.java))
             true
         }
+
+        weatherTask().execute()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -80,5 +92,56 @@ class MainActivity : AppCompatActivity() {
         return if (actionBarToggle.onOptionsItemSelected(item)) {
             true
         } else super.onOptionsItemSelected(item)
+    }
+
+    inner class weatherTask() : AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            /* Showing the ProgressBar, Making the main design GONE */
+            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
+            findViewById<LinearLayout>(R.id.weatherLayout).visibility = View.GONE
+            findViewById<TextView>(R.id.errorText).visibility = View.GONE
+        }
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response:String?
+            try{
+                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$weatherAPI").readText(
+                    Charsets.UTF_8
+                )
+            }catch (e: Exception){
+                response = null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                /* Extracting JSON returns from the API */
+                val jsonObj = JSONObject(result)
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+
+                val temp = main.getString("temp")+"°C"
+                val weatherDescription = weather.getString("description")
+                val currloc = jsonObj.getString("name")+", "+sys.getString("country")
+
+                /* Populating extracted data into our views */
+                findViewById<TextView>(R.id.location).text = currloc
+                findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
+                findViewById<TextView>(R.id.temp).text = temp
+
+                /* Views populated, Hiding the loader, Showing the main design */
+                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                findViewById<LinearLayout>(R.id.weatherLayout).visibility = View.VISIBLE
+
+            } catch (e: Exception) {
+                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
+            }
+
+        }
     }
 }
